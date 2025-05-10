38ab17fa0b3df216e95d862a385af55491cae4d3
Terry Gonguet <terry@gonguet.com>
2025-05-11 00:01:00 +0200

    Upgrade and use `safe()` to load content

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,11 +1,10 @@
+import { safe } from "@terrygonguet/utils/result"
+
 export const load = async ({ parent, depends }) => {
 	const { i18n } = await parent()
 	depends("content:home.md")
-	try {
-		const md = await import(`$content/${i18n.lang}/home.md`)
-		return { content: md.default as string }
-	} catch (error) {
-		const md = await import("$content/en/home.md")
-		return { content: md.default as string }
-	}
+	return safe(() => import(`$content/${i18n.lang}/home.md`))
+		.recover(() => import("$content/en/home.md"))
+		.andThen((module) => ({ content: module.default as string }))
+		.unwrap()
 }
